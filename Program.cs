34b303cf9b7a2e0e719cee9589306404eba5e781
Liz Kelley and Reese Lee <liz@>
2019-04-22 13:54:58 -0700

    add PalindromeChecker class and add for loop to that class, then call it in the Program Class to check if a user-inputted word is a palindrome

--- Program.cs
+++ Program.cs
@@ -1,6 +1,30 @@
+
 using System;
 
 namespace Palindrome {
+
+  public class PalindromeChecker
+  {
+    public bool isAPalindrome(string userWord)
+    {
+      // for (int os = 0; os < userWord.Length/2; os++)
+      // {
+      //   if (userWord[0 + os] != userWord[userWord.Length -1 - os])
+      //   {
+      //     return false;
+      //   }
+      // }
+      //
+      // return true;
+      for (int i = 0, j = userWord.Length -1; i < j; i++, j--) {
+        if (userWord[i] != userWord[j])
+        {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
 
   public class Program
   {
@@ -8,16 +32,21 @@ namespace Palindrome {
     {
       Console.WriteLine("Enter a word: ");
       string userWord = Console.ReadLine();
-      char[] userWordArray = userWord.ToCharArray();
-      Array.Reverse(userWordArray);
-      string backwards = new string(userWordArray);
 
-      Console.WriteLine(userWord == backwards);
+    PalindromeChecker pal = new PalindromeChecker();
+    bool isPal = pal.isAPalindrome(userWord);
+    Console.WriteLine(isPal);
 
-      // if (userWordReverse == userWordArray)
-      // {
-      //   Console.WriteLine("This is a palindrome");
-      // }
+
+      // Method to get palindrome using Array.Reverse
+      // char[] userWordArray = userWord.ToArray();
+      // Array.Reverse(userWordArray);
+      // string backwards = new string(userWordArray);
+      //
+      // Console.WriteLine(userWord == backwards);
+
+      //Method to get palindrome using for loops
+
     }
   }
 }
